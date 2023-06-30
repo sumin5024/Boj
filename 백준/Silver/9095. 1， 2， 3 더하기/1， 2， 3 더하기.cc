0413ff510c15c956d4a40513bc7567a764612ed3
sumin5024 <134565758+sumin5024@users.noreply.github.com>
2023-06-30 14:33:15 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,17 +1,9 @@
 #include <iostream>
-
 using namespace std;
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-    
+int main(){
 	int t; cin >> t;
-	
 	int arr[10] = { 1, 2, 4, 7, 13, 24, 44, 81, 149, 274 };
-	for (int i = 0; i < t; i++)
-	{
+	for (int i = 0; i < t; i++){
 		int n; cin >> n;
 		cout << arr[n-1] << '\n';
 	}
